376c97d4790682750f9bd9791d1e4064dc6a9d17
Artem <artem.xolod.90@gmail.com>
2023-12-10 20:41:37 +0300

    Настройка главной страница мессенджера v2

--- app/hooks/useRoutes.ts
+++ app/hooks/useRoutes.ts
@@ -1,2 +1,36 @@
 import {useMemo} from "react";
-import {useParams} from "next/navigation";
+import {usePathname} from "next/navigation";
+import {HiChat} from "react-icons/hi";
+import {HiArrowLeftOnRectangle, HiUsers} from "react-icons/hi2";
+import {signOut} from "next-auth/react";
+
+import useConversation from "@/app/hooks/useConversation";
+
+const useRoutes = () => {
+    const pathname = usePathname();
+    const {conversationId} = useConversation();
+
+
+    return useMemo(() => [
+        {
+            label: 'Chat',
+            href: '/conversation',
+            icon: HiChat,
+            active: pathname === '/conversation' || !!conversationId
+        },
+        {
+            label: 'Users',
+            href: '/users',
+            icon: HiUsers,
+            active: pathname === '/users'
+        },
+        {
+            label: 'Logout',
+            href: '#',
+            onClick: () => signOut(),
+            icon: HiArrowLeftOnRectangle
+        }
+    ], [pathname, conversationId])
+}
+
+export default useRoutes;
